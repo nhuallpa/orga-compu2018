164be3bbb949f0af240e8143ad67712177c5eccd
Nestor Huallpa <huallpa.nestor@gmail.com>
2018-10-30 02:51:19 -0300

    Completo informe. Faltaria stack frame de Decode

--- tp1/main.c
+++ tp1/main.c
@@ -122,13 +122,11 @@ int main (int argc, char** argv) {
     exit(1);
   }
   if (strcmp(p.accion, ENCODE) == 0) {
-    //codificar(archivoEntrada, archivoSalida);
     int fileDescriptorEntrada = fileno(archivoEntrada);
     int fileDescriptorSalida = fileno(archivoSalida);
 		returnCode = base64_encode(fileDescriptorEntrada, fileDescriptorSalida);
 		if (returnCode!=0) fprintf(stderr, "Error: %s \n", errmsg[returnCode]);
   } else if (strcmp(p.accion, DECODE) == 0) {
-    //decodificar(archivoEntrada, archivoSalida);
     int infd = fileno(archivoEntrada);
     int outfd = fileno(archivoSalida);
 		returnCode = base64_decode(infd, outfd);
